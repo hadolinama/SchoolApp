2158e45f44230c01bdc658933c865f59cb4dc8c4
Hilton Adolinama <hadolinama@outlook.com>
2017-11-01 18:06:52 -0700

    second

--- connection/connection.js
+++ connection/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "navrogeE47",
+  password: "",
   database: "school_db"
 });
 
